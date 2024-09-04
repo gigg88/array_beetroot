9a9dfe287a6545f120f142b09c2003833ad51898
gigg88 <popp.christian88@gmail.com>
2024-09-04 22:17:03 +0300

    x

---
+++ dist/main.dev.js
@@ -0,0 +1,52 @@
+"use strict";
+
+//Part 1: The Grocery List
+//   Create the Array:
+//        Start with an array named groceriesList.
+//       Each item is an object with properties:
+//          name (string)
+//           amount (number)
+//           bought (boolean, false initially)
+//        Add a few sample items to your list.
+//    Functions:
+//     displayGroceries():
+//          Prints the list to the console.
+//          Challenge: Sort so unbought items appear first.
+//      addItem(name, amount):
+//          Adds a new item or increases the quantity if it already exists.
+//       purchaseItem(name):
+//          Marks an item as bought.
+var groceriesList = [{
+  name: "Milk",
+  amount: 1,
+  bought: false
+}, {
+  name: "Eggs",
+  amount: 12,
+  bought: false
+}, {
+  name: "Bread",
+  amount: 1,
+  bought: false
+}];
+
+function displayGroceries() {
+  console.log(groceriesList);
+}
+
+displayGroceries();
+
+function addItem() {
+  var name = prompt("Enter the grocery name: ");
+  var amount = prompt("Enter the amount: ");
+  var newGrocery = {
+    name: name,
+    amount: amount,
+    bought: false
+  };
+  groceriesList.push(newGrocery);
+}
+
+addItem();
+
+function purchaseItem(name) {}
---
+++ main.js
@@ -0,0 +1,63 @@
+//Part 1: The Grocery List
+
+ //   Create the Array:
+//        Start with an array named groceriesList.
+ //       Each item is an object with properties:
+  //          name (string)
+ //           amount (number)
+ //           bought (boolean, false initially)
+//        Add a few sample items to your list.
+//    Functions:
+   //     displayGroceries():
+  //          Prints the list to the console.
+  //          Challenge: Sort so unbought items appear first.
+  //      addItem(name, amount):
+  //          Adds a new item or increases the quantity if it already exists.
+ //       purchaseItem(name):
+  //          Marks an item as bought.
+
+
+  const groceriesList = [
+
+  {
+    name: "Milk",
+    amount: 1,
+    bought: false
+  },
+
+  {
+    name: "Eggs",
+    amount: 12,
+    bought: false
+  },
+
+  {
+    name: "Bread",
+    amount: 1,
+    bought: false
+  }
+
+ ];
+
+
+ function displayGroceries() {
+    console.log(groceriesList);
+}
+
+displayGroceries();
+
+function addItem() {
+
+    const name = prompt("Enter the grocery name: ");
+    const amount = prompt("Enter the amount: ");
+
+    const newGrocery = { name, amount, bought: false };
+
+    groceriesList.push(newGrocery);
+}
+
+addItem();
+
+function purchaseItem(name) {
+    
+}
